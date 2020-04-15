472466330da8be332d60ecb51baa615069fb6cc5
Johan Winther <johan.winther@outlook.com>
2020-04-15 18:33:33 +0200

    Handle missing games folder

--- source/server/game-handler.ts
+++ source/server/game-handler.ts
@@ -6,12 +6,17 @@ const gameHandler = {
 }
 
 function findGames() {
-    let games = (
-        fs.readdirSync(path.join(__dirname, 'games'), { withFileTypes: true })
-            .filter(dirent => dirent.isDirectory())
-            .filter(dirent => fs.existsSync(path.join(__dirname, 'games', dirent.name, 'game.json')))
-            .map(dirent => `/games/${dirent.name}/`)
-    )
+    let games: string[];
+    if (fs.existsSync(path.join(__dirname, 'games'))) {
+        games = (
+            fs.readdirSync(path.join(__dirname, 'games'), { withFileTypes: true })
+                .filter(dirent => dirent.isDirectory())
+                .filter(dirent => fs.existsSync(path.join(__dirname, 'games', dirent.name, 'game.json')))
+                .map(dirent => `/games/${dirent.name}/`)
+        )
+    } else {
+        games = []
+    }
     return games
 }
 
