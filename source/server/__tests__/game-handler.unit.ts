d9a155e505dc1bf230ac91fdc4b3c1e71bf72169
Johan Winther <johan.winther@outlook.com>
2020-04-18 02:02:59 +0200

    Removes duplicate test

--- source/server/__tests__/game-handler.unit.ts
+++ source/server/__tests__/game-handler.unit.ts
@@ -14,10 +14,3 @@ test('findGames returns empty string array if games folder is empty', () => {
     
     expect(gameHandler.findGames()).toStrictEqual([] as string[]);
 });
-
-test('findGames returns empty string array if games folder is empty', () => {
-    fs.existsSync = jest.fn().mockReturnValueOnce(true); // games folder exists
-    fs.readdirSync = jest.fn().mockReturnValue([] as Dirent[]); // no entries in games folder
-
-    expect(gameHandler.findGames()).toStrictEqual([] as string[]);
-});
