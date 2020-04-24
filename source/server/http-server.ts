27cc62f1ec9b87ea31f451869dda4238970a7520
Johan Winther <johan.winther@outlook.com>
2020-04-24 17:24:01 +0200

    Formatting changes

--- source/server/http-server.ts
+++ source/server/http-server.ts
@@ -1,27 +1,16 @@
 import express from 'express';
 import path from 'path';
-import gameHandler from './game-handler';
 
 const httpServer = express();
-const games = gameHandler.findGames();
-console.log(games);
 
 httpServer.use(
     '/',
-    express.static(
-        path.join(__dirname, 'client')
-    )
-)
+    express.static(path.join(__dirname, 'client'))
+);
 
 httpServer.use(
     '/games',
-    express.static(
-        path.join(__dirname, 'games')
-    )
-)
-
-httpServer.get('/g', (req, res) => {
-    res.json(games);
-});
+    express.static(path.join(__dirname, 'games'))
+);
 
 export = httpServer
