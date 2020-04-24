67593a009c353b892f31b0709c8f7476973b36d4
Johan Winther <johan.winther@outlook.com>
2020-04-24 17:24:13 +0200

    Load games at start

--- source/server/index.ts
+++ source/server/index.ts
@@ -1,7 +1,16 @@
 import httpServer from './http-server';
+import GameHandler from './game-handler';
+
 
 const HTTP_PORT: number = Number(process.env.PORT) || 80;
 const WEBSOCKET_PORT: number = Number(process.env.WSPORT) || 3000;
+
+const gameHandler = new GameHandler();
+gameHandler.loadGames(gameHandler.findGames());
+
+httpServer.get('/api/games', (req, res) => {
+    res.json(gameHandler.games);
+});
 
 // Start the Express server
 httpServer.listen(HTTP_PORT, () => {
