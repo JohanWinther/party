4058926f2031f9a225ddfc987e7c36873bfc9f96
Johan Winther <johan.winther@outlook.com>
2020-04-24 16:45:53 +0200

    GameHandler can find and load game info

--- source/server/__tests__/game-handler.unit.ts
+++ source/server/__tests__/game-handler.unit.ts
@@ -1,18 +1,213 @@
+import { mocked } from 'ts-jest/utils';
 jest.mock('fs');
 
-import fs, { Dirent } from 'fs';
-import gameHandler from '../game-handler';
+import fs from 'fs';
+import path from 'path';
+import GameHandler from '../game-handler';
 
+describe('GameHandler', () => {
 
-test('findGames returns empty string array if game folder is missing', () => {
-    fs.existsSync = jest.fn().mockReturnValue(false);
+    it('should have empty game array upon creation', () => {
+        const gameHandler = new GameHandler();
+    
+        expect(gameHandler.games).toStrictEqual([]);
+    });
 
-    expect(gameHandler.findGames()).toStrictEqual([] as string[]);
 });
 
-test('findGames returns empty string array if games folder is empty', () => {
-    fs.existsSync = jest.fn().mockReturnValueOnce(true); // games folder exists
-    fs.readdirSync = jest.fn().mockReturnValue([] as Dirent[]); // no entries in games folder
+describe('findGames', () => {
+
+    let gameHandler: GameHandler;
+    const gamesFolderPath: fs.PathLike = path.join(__dirname, '../', 'games');
+
+    beforeEach(() => {
+        gameHandler = new GameHandler();
+    });
+
+    it('should return empty array if games folder does not exist', () => {
+        mocked(fs.readdirSync).mockImplementation((inputPath: fs.PathLike): fs.Dirent[] => {
+                throw new Error(`Uncaught Error: ENOENT: no such file or directory, scandir '${inputPath}'`)
+        });
+
+        expect(gameHandler.findGames()).toStrictEqual([]);
+    });
+
+    it('should return empty array if games folder is empty', () => {
+        mocked(fs.readdirSync).mockReturnValue([] as fs.Dirent[]);
+
+        expect(gameHandler.findGames()).toStrictEqual([]);
+    });
+
+    it('should return empty array if games folder does not have any folders', () => {
+        const fileDirent = new fs.Dirent();
+        fileDirent.isFile = (): boolean => true;
+        fileDirent.isDirectory = (): boolean => false;
+        mocked(fs.readdirSync).mockReturnValue([fileDirent]);
+
+        expect(gameHandler.findGames()).toStrictEqual([]);
+    });
+
+    it('should return empty array if games folder has folders without game.json', () => {
+        const fileDirent = new fs.Dirent();
+        fileDirent.isFile = (): boolean => true;
+        fileDirent.isDirectory = (): boolean => false;
+
+        const folderDirent = new fs.Dirent();
+        folderDirent.isFile = (): boolean => false;
+        folderDirent.isDirectory = (): boolean => true;
+        folderDirent.name = 'example.game';
+
+        const existingPaths: fs.PathLike[] = [
+            path.join(gamesFolderPath, folderDirent.name),
+        ];
+
+        mocked(fs.readdirSync).mockReturnValue([folderDirent, fileDirent]);
+        mocked(fs.existsSync).mockImplementation((inputPath: fs.PathLike): boolean => 
+            existingPaths.includes(inputPath)
+        );
+
+        expect(gameHandler.findGames()).toStrictEqual([]);
+    });
+
+    it('should return one path if games folder has one folder with game.json', () => {
+        const fileDirent = new fs.Dirent();
+        fileDirent.isFile = (): boolean => true;
+        fileDirent.isDirectory = (): boolean => false;
+
+        const folderDirent = new fs.Dirent();
+        folderDirent.isFile = (): boolean => false;
+        folderDirent.isDirectory = (): boolean => true;
+        folderDirent.name = 'example.game';
+
+        const existingPaths: fs.PathLike[] = [
+            path.join(gamesFolderPath, folderDirent.name),
+            path.join(gamesFolderPath, folderDirent.name, 'game.json'),
+        ];
+
+        mocked(fs.readdirSync).mockReturnValue([folderDirent, fileDirent]);
+        mocked(fs.existsSync).mockImplementation((inputPath: fs.PathLike): boolean =>
+            existingPaths.includes(inputPath)
+        );
+
+        expect(gameHandler.findGames()).toStrictEqual([
+            path.join(gamesFolderPath, folderDirent.name, 'game.json')
+        ]);
+    });
+
+
+    it('should return one path if games folder has one folder with game.json and one without', () => {
+        const fileDirent = new fs.Dirent();
+        fileDirent.isFile = (): boolean => true;
+        fileDirent.isDirectory = (): boolean => false;
+
+        const folderDirent1 = new fs.Dirent();
+        folderDirent1.isFile = (): boolean => false;
+        folderDirent1.isDirectory = (): boolean => true;
+        folderDirent1.name = 'example.game1';
+
+        const folderDirent2 = new fs.Dirent();
+        folderDirent2.isFile = (): boolean => false;
+        folderDirent2.isDirectory = (): boolean => true;
+        folderDirent2.name = 'example.game2';
+
+        const existingPaths: fs.PathLike[] = [
+            path.join(gamesFolderPath, folderDirent1.name),
+            path.join(gamesFolderPath, folderDirent1.name, 'game.json'),
+            path.join(gamesFolderPath, folderDirent2.name),
+        ];
+
+        mocked(fs.readdirSync).mockReturnValue([folderDirent1, folderDirent2, fileDirent]);
+        mocked(fs.existsSync).mockImplementation((inputPath: fs.PathLike): boolean =>
+            existingPaths.includes(inputPath)
+        );
+
+        expect(gameHandler.findGames()).toStrictEqual([
+            path.join(gamesFolderPath, folderDirent1.name, 'game.json')
+        ]);
+    });
+
+    it('should return two paths if games folder has two folders with game.json', () => {
+        const fileDirent = new fs.Dirent();
+        fileDirent.isFile = (): boolean => true;
+        fileDirent.isDirectory = (): boolean => false;
+
+        const folderDirent1 = new fs.Dirent();
+        folderDirent1.isFile = (): boolean => false;
+        folderDirent1.isDirectory = (): boolean => true;
+        folderDirent1.name = 'example.game1';
+
+        const folderDirent2 = new fs.Dirent();
+        folderDirent2.isFile = (): boolean => false;
+        folderDirent2.isDirectory = (): boolean => true;
+        folderDirent2.name = 'example.game2';
+
+        const existingPaths: fs.PathLike[] = [
+            path.join(gamesFolderPath, folderDirent1.name),
+            path.join(gamesFolderPath, folderDirent1.name, 'game.json'),
+            path.join(gamesFolderPath, folderDirent2.name),
+            path.join(gamesFolderPath, folderDirent2.name, 'game.json'),
+        ];
+
+        mocked(fs.readdirSync).mockReturnValue([folderDirent1, folderDirent2, fileDirent]);
+        mocked(fs.existsSync).mockImplementation((inputPath: fs.PathLike): boolean =>
+            existingPaths.includes(inputPath)
+        );
+
+        expect(gameHandler.findGames()).toStrictEqual([
+            path.join(gamesFolderPath, folderDirent1.name, 'game.json'),
+            path.join(gamesFolderPath, folderDirent2.name, 'game.json')
+        ]);
+    });
+
+});
+
+describe('loadGames', () => {
+
+    let gameHandler: GameHandler;
+    const gamesFolderPath: fs.PathLike = path.join(__dirname, '../', 'games');
+
+    beforeEach(() => {
+        gameHandler = new GameHandler();
+    });
+
+    it('should make games property an empty array if no paths', () => {
+        gameHandler.loadGames([]);
+        expect(gameHandler.games).toStrictEqual([]);
+    });
+
     
-    expect(gameHandler.findGames()).toStrictEqual([] as string[]);
+    it('should make games property an empty array if file at path does not exist', () => {
+        mocked(fs.readFileSync).mockImplementation((inputPath: fs.PathLike): string => {
+            throw new Error(`Uncaught Error: ENOENT: no such file or directory, scandir '${inputPath}'`);
+        });
+
+        gameHandler.loadGames([
+            path.join(gamesFolderPath, 'example.game', 'game.json')
+        ]);
+        expect(gameHandler.games).toStrictEqual([]);
+    });
+
+    it('should make games property an empty array if game.json is not valid JSON', () => {
+        mocked(fs.readFileSync).mockReturnValue("not a valid json string");
+
+        gameHandler.loadGames([
+            path.join(gamesFolderPath, 'example.game', 'game.json')
+        ]);
+        expect(gameHandler.games).toStrictEqual([]);
+    });
+
+    it('should make games property an empty array if game.json is missing required keys', () => {
+        const gameData = {
+            title: "Example Title",
+        }
+        mocked(fs.readFileSync).mockReturnValue(JSON.stringify(gameData));
+
+        gameHandler.loadGames([
+            path.join(gamesFolderPath, 'example.game', 'game.json')
+        ]);
+        expect(gameHandler.games).toStrictEqual([]);
+    });
+
+    /* Add tests for checking missing keys and wrong types */
+
 });
--- source/server/game-handler.ts
+++ source/server/game-handler.ts
@@ -1,24 +1,153 @@
 import fs from 'fs';
 import path from 'path';
 
+class Game {
 
-function findGames(): string[] {
-    let games: string[];
-    if (fs.existsSync(path.join(__dirname, 'games'))) {
-        games = (
-            fs.readdirSync(path.join(__dirname, 'games'), { withFileTypes: true })
-                .filter(dirent => dirent.isDirectory())
-                .filter(dirent => fs.existsSync(path.join(__dirname, 'games', dirent.name, 'game.json')))
-                .map(dirent => `/games/${dirent.name}/`)
-                )
-            } else {
-                games = [];
-            }
-            return games
+    [key: string]: (
+        string |
+        boolean |
+        URL |
+        { name: string; link?: URL } |
+        { min: number | null; max: number | null }
+    );
+
+    isValid: boolean;
+    title: string;
+    description: string;
+    author: {
+        name: string;
+        link?: URL;
+    }
+    version: string;
+    supportLink: URL;
+    numberOfPlayers: {
+        min: number | null;
+        max: number | null;
+    };
+    playTime: {
+        min: number | null;
+        max: number | null;
+    };
+    explicitContent: boolean;
+    audienceSupported: boolean;
+
+    constructor(gameInfoPath: fs.PathLike) {
+        this.isValid = this.parseGameInfoFile(gameInfoPath);
+    }
+
+    private parseGameInfoFile(gameInfoPath: fs.PathLike): boolean {
+        let gameData;
+        try {
+            const fileText = fs.readFileSync(gameInfoPath, { encoding: 'utf-8' });
+            gameData = JSON.parse(fileText);
+        } catch (e) {
+            return false;
         }
-        
-const gameHandler = {
-    "findGames": findGames
+
+        if (gameData.title && typeof gameData.title === 'string') {
+            this['title'] = gameData['title'];
+            this.title = gameData.title;
+        } else {
+            return false;
+        }
+
+        if (gameData.description && typeof gameData.description === 'string') {
+            this.description = gameData.description;
+        } else {
+            return false;
+        }
+
+        if (gameData.author &&
+            typeof gameData.author === 'object' && 
+            gameData.author.name &&
+            typeof gameData.author.name === 'string') {
+            
+            this.author.name = gameData.author.name;
+
+            if (gameData.author.link && typeof gameData.author.link === 'string') {
+                this.author.link = gameData.author.link;
+            }
+            
+        } else {
+            return false;
+        }
+
+        if (gameData.version && typeof gameData.version === 'string') {
+            this.version = gameData.version;
+        } else {
+            return false;
+        }
+
+        if (gameData.supportLink && typeof gameData.supportLink === 'string') {
+            this.supportLink = gameData.supportLink;
+        }
+
+        if (gameData.numberOfPlayers &&
+            typeof gameData.numberOfPlayers === 'object') {
+        /* Implement check later */
+            this.numberOfPlayers = gameData.numberOfPlayers;
+        } else {
+            return false;
+        }
+
+        if (gameData.playTime &&
+            typeof gameData.playTime === 'object') {
+            /* Implement check later */
+            this.playTime = gameData.playTime;
+        } else {
+            return false;
+        }
+
+        if (gameData.explicitContent !== undefined &&
+            typeof gameData.explicitContent === 'boolean') {
+            this.explicitContent = gameData.explicitContent;
+        } else {
+            return false;
+        }
+
+        if (gameData.audienceSupported !== undefined &&
+            typeof gameData.audienceSupported === 'boolean') {
+            this.audienceSupported = gameData.audienceSupported;
+        } else {
+            return false;
+        }
+
+        return true;
+    }
 }
-        
-export = gameHandler
+
+class GameHandler {
+    games: Game[];
+
+    constructor() {
+        this.games = [];
+    }
+
+    findGames(): fs.PathLike[] {
+        const gamesFolderPath: fs.PathLike = path.join(__dirname, 'games');
+
+        let direntsInGamesFolder: fs.Dirent[];
+        try {
+            direntsInGamesFolder = fs.readdirSync(gamesFolderPath, { withFileTypes: true });
+        } catch (e) {
+            return [];
+        }
+        const foldersInGamesFolder = direntsInGamesFolder.filter(dirent => dirent.isDirectory());
+        if (foldersInGamesFolder.length === 0) {
+            return [];
+        }
+        const gameInfoFilePaths: fs.PathLike[] = foldersInGamesFolder
+            .map(dirent => path.join(gamesFolderPath, dirent.name, 'game.json'))
+            .filter(gameInfoPath => fs.existsSync(gameInfoPath));
+
+        return gameInfoFilePaths
+    }
+
+    loadGames(gameInfoFilePaths: fs.PathLike[]): void {
+        this.games = gameInfoFilePaths
+            .map(gameInfoPath => new Game(gameInfoPath))
+            .filter(game => game.isValid);
+    }
+}
+
+export = GameHandler
