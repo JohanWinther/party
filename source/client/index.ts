867b387b088c6f0821d2d682747fb84bae210a28
Johan Winther <johan.winther@outlook.com>
2020-04-25 11:01:55 +0200

    Show game list at startup

--- source/client/index.ts
+++ source/client/index.ts
@@ -1,1 +1,19 @@
 console.log("Screen client");
+
+function main(): void {
+
+    const gamesList = document.getElementById('games');
+
+    fetch('/games/')
+        .then(res => res.json())
+        .then(games => {
+            gamesList.innerHTML = games.map((game) => (`
+                <li tabindex="0"><a href="${game.url}" tabindex="-1">${game.title}</a></li>
+            `));
+        });
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    main()
+});
