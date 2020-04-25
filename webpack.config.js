df3f7885b6b1e4dc29e02c367ef71bb332e1c9aa
Johan Winther <johan.winther@outlook.com>
2020-04-26 01:06:27 +0200

    Add module building configuration
    We want to export a library for the SocketClient which games can import.
    That module will be separately built and excluded from the bundle file.

--- webpack.config.js
+++ webpack.config.js
@@ -83,6 +83,9 @@ const getClientConfig = env => {
             filename: '[name].js',
             path: path.join(__dirname, 'build', env.buildFolder, 'client'),
         },
+        externals: {
+            "socket-client": 'SocketClient'
+        },
         plugins: [
             new ForkTsCheckerWebpackPlugin(),
             new CopyPlugin([
@@ -99,15 +102,59 @@ const getClientConfig = env => {
     };
 };
 
+const getLibraryConfig = env => {
+    return {
+        target: "web",
+        entry: {
+            "socket-client": './source/client/socket-client.ts',
+        },
+        mode: env.NODE_ENV,
+        devtool: env.devtool,
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true, // type checking used in plugin
+                            experimentalWatchApi: true,
+                            compilerOptions: {
+                                target: "ES2019",
+                                module: "ES6",
+                                removeComments: env.isProduction,
+                                preserveConstEnums: !env.isProduction,
+                                sourceMap: !env.isProduction
+                            }
+                        }
+                    }
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: '[name].js',
+            libraryTarget: 'umd',
+            path: path.join(__dirname, 'build', env.buildFolder, 'client'),
+        },
+        plugins: [
+            new ForkTsCheckerWebpackPlugin(),
+        ]
+    };
+};
+
 module.exports = env => {
 
     // Environment
     env.isProduction = env.NODE_ENV === 'production';
     env.devtool = env.isProduction ? false : 'inline-source-map';
     env.buildFolder = env.isProduction ? 'release' : 'development';
-    
+
     return [
         getServerConfig(env),
-        getClientConfig(env)
+        getClientConfig(env),
+        getLibraryConfig(env),
     ]
 };
