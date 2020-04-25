b77273ec5522f31a9805e824cd3b1df455593f04
Johan Winther <johan.winther@outlook.com>
2020-04-25 13:59:45 +0200

    Game interface in client

--- source/client/index.ts
+++ source/client/index.ts
@@ -1,5 +1,28 @@
 console.log("Screen client");
 
+interface Game {
+    url: string;
+    isValid: boolean;
+    title: string;
+    description: string;
+    author: {
+        name: string;
+        link?: URL;
+    };
+    version: string;
+    supportLink: URL;
+    numberOfPlayers: {
+        min: number | null;
+        max: number | null;
+    };
+    playTime: {
+        min: number | null;
+        max: number | null;
+    };
+    explicitContent: boolean;
+    audienceSupported: boolean;
+}
+
 function main(): void {
 
     const gamesList = document.getElementById('games');
@@ -7,13 +30,36 @@ function main(): void {
     fetch('/games/')
         .then(res => res.json())
         .then(games => {
-            gamesList.innerHTML = games.map((game) => (`
-                <li tabindex="0"><a href="${game.url}" tabindex="-1">${game.title}</a></li>
+            gamesList.innerHTML = games.map((game: Game) => (`
+                <li tabindex="0" title="${escape(game.description)}"><a href="${game.url}" tabindex="-1">${game.title}</a></li>
             `));
         });
 
 }
 
+function connect(): void {
+
+    console.log('Connecting to websocket..');
+    const ws = new WebSocket(`ws://${location.hostname}:3000`);
+
+    ws.onopen = (): void => {
+        console.log('Connected to websocket');
+        const clientId = (new Date()).getTime()*100 + Math.floor(Math.random()*100);
+        ws.send(clientId.toString());
+        console.log(`Sending ${clientId}`);
+    };
+    ws.onclose = (): void => {
+        setTimeout(() => {
+            connect();
+        }, 2000);
+    };
+    ws.onmessage = (ev): void => {
+        console.log(ev.data);
+    };
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     main()
+    connect();
 });
