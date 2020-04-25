23b82db22fd9deb53299eb957bc0ea834f0c6601
Johan Winther <johan.winther@outlook.com>
2020-04-25 14:00:07 +0200

    Create SocketLink on startup

--- source/server/index.ts
+++ source/server/index.ts
@@ -1,5 +1,6 @@
 import httpServer from './http-server';
 import GameHandler from './game-handler';
+import SocketLink from './socket-link';
 
 
 const HTTP_PORT: number = Number(process.env.PORT) || 80;
@@ -7,6 +8,8 @@ const WEBSOCKET_PORT: number = Number(process.env.WSPORT) || 3000;
 
 const gameHandler = new GameHandler();
 gameHandler.loadGames(gameHandler.findGames());
+
+const socketLink = new SocketLink(WEBSOCKET_PORT);
 
 httpServer.get('/games/', (req, res) => {
     res.json(gameHandler.games);
